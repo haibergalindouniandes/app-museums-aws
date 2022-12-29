7eb71a2a6ed9eb2eb5bb08a57fba56ec2568555a
Haiber H. Galindo <h.galindos@uniandes.edu.co>
2022-12-29 11:07:07 -0500

    Proyecto inicial, data almacenada en memoria

--- configs/config.js
+++ configs/config.js
@@ -2,5 +2,5 @@ module.exports = {
   privateKey: '16b8318a-864b-11ed-a1eb-0242ac120002',
   defaultUser: 'zlaifer619',
   defaultPassword: 'Zl@ifer619',
-  expireTimeToken: 60,
+  expireTimeToken: 60 * 10,
 }
